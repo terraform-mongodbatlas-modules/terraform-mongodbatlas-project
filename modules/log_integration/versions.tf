cca4c9df2b7a10f5d7d6f0cb787200bc8f61b5db
Leo Antoli <430982+lantoli@users.noreply.github.com>
2026-07-23 15:57:50 +0200

    chore: Prepare Project provider version matrix testing

--- modules/log_integration/versions.tf
+++ modules/log_integration/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.9"
+  required_version = ">= 1.10"
 
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "~> 2.8"
+      version = "~> 2.14"
     }
   }
 
--- tests/workspace_project_examples/main.tf
+++ tests/workspace_project_examples/main.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     mongodbatlas = {
       source  = "mongodb/mongodbatlas"
-      version = "~> 2.8"
+      version = "~> 2.14"
     }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
     }
   }
-  required_version = ">= 1.9"
+  required_version = ">= 1.10"
 }
 
 provider "mongodbatlas" {}
